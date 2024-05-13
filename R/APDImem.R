374ab44750ab8509148315bed29a409e8a1deaac
Pei Zhang <121835148+BettyzhangPei@users.noreply.github.com>
2024-05-13 18:04:32 -0400

    Update APDImem.R

--- R/APDImem.R
+++ R/APDImem.R
@@ -105,6 +105,8 @@ NME<- function(Q.N.1, Q.N.2, Q.E.1, Q.E.2, F.N.1, F.N.2, F.E.1, F.E.2)
   # Total number of individuals:
   I <- dim(D)[2]
 
+   NME1<- function(D)
+  {
 
   est.theta<- rep(0,15)
 
@@ -173,41 +175,99 @@ NME<- function(Q.N.1, Q.N.2, Q.E.1, Q.E.2, F.N.1, F.N.2, F.E.1, F.E.2)
   est.coeff<- data.frame(est.coeff)
   rownames(est.coeff) <- c("Rho.N", "Lambda.N", "Rho.E",  "Lambda.E", "Rho.R",  "Lambda.R")
 
-
-
-  
-  # other unknown fixed parameters in the model:
-  est.fix.par<- rep(0,10)
-
-  # estimated beta_N0_1:
-  est.fix.par[1]<-  mean(D[1,]) - est.fix.par[1]*est.fix.par[3]
-  #  estimated beta_N0_2:
-  est.fix.par[2]<- mean(D[2,]) - est.fix.par[1]*est.fix.par[3]
-  # estimated beta_N1:
-  est.fix.par[3]<- est.theta[5]/est.theta[12]
-  # estimated beta_E0_1:
-  est.fix.par[4]<- mean(D[3,]) - est.fix.par[2]*est.fix.par[4]
-  # estimated beta_E0_2:
-  est.fix.par[5]<- mean(D[4,]) -est.fix.par[2]*est.fix.par[4]
-  # estimated beta_E1:
-  est.fix.par[6]<- est.theta[10]/est.theta[15]
-  # estimated mu_T_N:
-  est.fix.par[7]<- mean(D[5,])
-  # estimated mu_T_E:
-  est.fix.par[8]<- mean(D[7,])
-  # estimated mu_N0_2:
-  est.fix.par[9]<- mean(D[6,]) - mean(D[5,])
-  # estimated mu_E0_2:
-  est.fix.par[10]<- mean(D[8,]) - mean(D[7,])
-
-  
+     # other unknown fixed parameters in the model:
+    est.fix.par<- rep(0,10)
+    
+    # estimated beta_N0_1:
+    est.fix.par[1]<-  mean(D[1,]) - est.fix.par[1]*est.fix.par[3]
+    #  estimated beta_N0_2:
+    est.fix.par[2]<- mean(D[2,]) - est.fix.par[1]*est.fix.par[3]
+    # estimated beta_N1:
+    est.fix.par[3]<- est.theta[5]/est.theta[12]
+    # estimated beta_E0_1:
+    est.fix.par[4]<- mean(D[3,]) - est.fix.par[2]*est.fix.par[4]
+    # estimated beta_E0_2:
+    est.fix.par[5]<- mean(D[4,]) -est.fix.par[2]*est.fix.par[4]
+    # estimated beta_E1:
+    est.fix.par[6]<- est.theta[10]/est.theta[15]
+    # estimated mu_T_N:
+    est.fix.par[7]<- mean(D[5,])
+    # estimated mu_T_E:
+    est.fix.par[8]<- mean(D[7,])
+    # estimated mu_N0_2:
+    est.fix.par[9]<- mean(D[6,]) - mean(D[5,])
+    # estimated mu_E0_2:
+    est.fix.par[10]<- mean(D[8,]) - mean(D[7,])
+     
   est.fix.par<- data.frame(est.fix.par)
   
   rownames(est.fix.par) <- c("Beta.N0.1", "Beta.N0.2", "Beta.N1", "Beta.E0.1","Beta.E0.2", "Beta.E1", "Mu.T.N",  "Mu.T.E", "Mu.N0.2", "Mu.E0.2")
 
-  return(list(est.coefficients= as.matrix(est.coeff)))
-  #return(list(est.coefficients= as.matrix(est.coeff), est.covariance.matrix= est.cov.mat, est.fix.parameters= as.matrix(est.fix.par)))
+  return(list(est.coefficients= as.matrix(est.coeff), est.theta= as.matrix(est.theta), est.covariance.matrix= est.cov.mat, est.fix.parameters= as.matrix(est.fix.par)))
+}
 
+# I = number of individuals
+  # fixed effects in the measurement error model:
+  # beta = (beta_N0_1, beta_N0_2, beta_N1, beta_E0_1, beta_E0_2, beta_E1)
+  # mu_1 = (mu_T_N, mu_T_E, mu_N0_2, mu_E0_2)
+  # est.coefficients: estimated correlation coefficients and attenuation factors
+  ex<- NME1(D)
+  beta<- as.numeric(ex$est.fix.parameters[1:6])
+  mu_1<- as.numeric(ex$est.fix.parameters[7:10])
+  theta<- as.numeric(ex$est.theta)
+
+# mu = first moments:  the mean vector of D_i
+  # second moments:
+  # theta = (theta1, theta2, ..., theta15)
+  mu<- rep(0,8)
+  mu[1]<- beta[1] + beta[3]*mu_1[1]
+  mu[2]<- beta[2] + beta[3]*mu_1[1]
+  mu[3]<- beta[4] + beta[6]*mu_1[2]
+  mu[4]<- beta[5] + beta[6]*mu_1[2]
+  mu[5]<- mu_1[1]
+  mu[6]<- mu_1[3] + mu_1[1]
+  mu[7]<- mu_1[2]
+  mu[8]<- mu_1[4] + mu_1[2]
+  
+  
+  
+  theta_set<- c(theta[1], theta[2], theta[3], theta[4], theta[5], theta[5], theta[6], theta[6], theta[2], theta[1], theta[4], theta[3], theta[5], theta[5], theta[6], theta[6],
+                theta[3], theta[4], theta[7], theta[8], theta[9], theta[9], theta[10], theta[10], theta[4], theta[3], theta[8], theta[7], theta[9], theta[9], theta[10], theta[10],
+                theta[5], theta[5], theta[9], theta[9], theta[11], theta[12], theta[13],theta[13],
+                theta[5], theta[5], theta[9], theta[9], theta[12], theta[11], theta[13],theta[13],
+                theta[6], theta[6], theta[10], theta[10], theta[13], theta[13], theta[14], theta[15], theta[6], theta[6],theta[10], theta[10], theta[13], theta[13], theta[15], theta[14])
+  # variance covariance matrix of D_i
+  Sigma<- matrix(theta_set, 8, 8)
+  
+  M<-1000 # number of repetitions
+  coefficients_M<- matrix(0, 6, M )
+  
+  for (a in 1: M) 
+  {
+    set.seed(a)
+    D2<- matrix(0, nrow=8 , ncol=I)
+    D2<- t(as.matrix(mvrnorm(I, mu, Sigma)))
+    ex1<- NME1(D2)
+    coefficients_M[, a]<- as.numeric(ex1$est.coefficients)
+  }
+  
+  CI<- matrix(0, nrow=6, ncol=3)
+  for (i in 1:6)
+  {
+    CI[i, 2]<- ex$est.coefficients[i] - 1.96 *sd(coefficients_M[i,])
+    CI[i, 3]<-  ex$est.coefficients[i] + 1.96 *sd(coefficients_M[i,])
+  }
+  
+  CI[,1]<- as.numeric(ex$est.coefficients)
+  
+  CI<- data.frame(CI)
+  rownames(CI)<-  c("Rho.N", "Lambda.N", "Rho.E",  "Lambda.E", "Rho.R",  "Lambda.R")
+  colnames(CI)<-  c("Estimation", "lower bound of 95% CI", "upper bound of 95% CI")
+  
+  return(list(CI= as.matrix(CI)))
+}
+
+  
 }
 
 
